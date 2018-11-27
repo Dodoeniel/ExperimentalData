54ceb5e17a2d87ea9ed4dec0d5e94654419849eb
Dodoeniel <daniel.schoepflin@ tuhh.de>
2018-11-27 14:34:00 +0100

    RasendeGurkeUpload

--- Libraries/data_preprocessing.py
+++ Libraries/data_preprocessing.py
@@ -461,16 +461,65 @@ def getTimeDistributedLabels(eec_data,X_ts, flag=0):
 
 
 def sliceData(X_ts, labels, w_length, hop_size, discard=1, type=1):
-
+    # pre define output
+    X_sliced = pd.DataFrame()
+    labels_sliced = pd.DataFrame()
     for stopId in X_ts['stopId'].unique():
+        # get the one series that shall be sliced
         X_snippet = X_ts.loc[X_ts['stopId'] == stopId]
         label_snippet = labels.loc[labels['stopId'] == stopId]
-        X_sliced = pd.DataFrame()
-        labels_sliced = pd.DataFrame()
-        if len(X_snippet) >= w_length:
-            hop = 0;
-            while True:
-                X_sliced.append(X_snippet[hop*hop_size:hop*hop_size+w_length])
+
+        if len(X_snippet) >= w_length or (len(X_snippet) < w_length and discard == 0):
+            hop = 0
+            for hop in range((len(X_snippet)-w_length)//hop_size):
+
+                # create Ids for sliced TS:  1.051_0 .... 1.051_0 1.051_1 .... 1.051_1
+                sliceId = []
+                for i in range(w_length):
+                    sliceId.append(stopId + '_' + str(hop))
+
+                # slice labels
+                curr_label = label_snippet[hop * hop_size:hop * hop_size + w_length]
+                curr_label.index = range(w_length)
+                curr_label['sliceId'] = pd.Series(sliceId, index=curr_label.index)
+                labels_sliced = pd.concat([labels_sliced, curr_label])
+
+                # slice data
+                curr_X = X_snippet[hop * hop_size:hop * hop_size + w_length]
+                curr_X.index = range(w_length)
+                curr_X['sliceId'] = pd.Series(sliceId, index=curr_X.index)
+                X_sliced = pd.concat([X_sliced, curr_X])
+
+            if (len(X_snippet)-w_length) % hop_size > 0 & discard == 0: # remainder of windowed function
+
+                if hop != 0:
+                    hop += 1
+                    curr_label = label_snippet[hop * hop_size + w_length:]
+                    curr_X = X_snippet[hop * hop_size + w_length:]
+                else:
+                    curr_label = label_snippet
+                    curr_X = X_snippet
+
+                sliceId = stopId + '_' + str(hop)
+
+                curr_label.index = range(len(curr_label))
+                zero_padding = pd.DataFrame(np.zeros(((w_length - len(curr_label)), curr_label.shape[1])),
+                                            index=range(len(curr_label), w_length),
+                                            columns=curr_label.columns.values.tolist())
+                curr_label = pd.concat([curr_label, zero_padding])
+                curr_label['sliceId'] = pd.Series(sliceId, index=curr_label.index)
+                labels_sliced = pd.concat([labels_sliced, curr_label])
+
+                # remaining data
+                curr_X.index = range(len(curr_X))
+
+                zero_padding = pd.DataFrame(np.zeros(((w_length - len(curr_X)), curr_X.shape[1])),
+                                            index=range(len(curr_X), w_length),
+                                            columns=curr_X.columns.values.tolist())
+                curr_X = pd.concat([curr_X, zero_padding])
+                curr_X['sliceId'] = pd.Series(sliceId, index=curr_X.index)
+                X_sliced = pd.concat([X_sliced, curr_X])
+    return X_sliced, labels_sliced
 
 
 
