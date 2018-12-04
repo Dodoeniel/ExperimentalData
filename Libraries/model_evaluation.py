b15ce449598bdbe4ad4fcf3f4256502a15a5eec3
Daniel Schoepflin <daniel.schoepflin@tuhh.de>
2018-12-04 17:49:44 +0100

    DanielStart

--- Libraries/model_evaluation.py
+++ Libraries/model_evaluation.py
@@ -2,6 +2,8 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import math
+import numpy as np
+from matplotlib2tikz import save as tikz_save
 
 def calculateConfusionMatrix(labels, predictions):
     confusion = tf.confusion_matrix(labels, predictions)
@@ -15,3 +17,17 @@ def calculateMCC(confusionMatrix):
     fn = confusionMatrix[1,0]
     mcc = (tp*tn - fp*fn) / math.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
     return mcc
+
+def plot_loss_history(histories, epochs, name):
+    val_loss = np.zeros((epochs, 1))
+    train_loss = np.zeros((epochs, 1))
+    for history in histories:
+        val_loss = val_loss + np.array(history.history['val_loss'])
+        train_loss = train_loss + np.array(history.history['loss'])
+    plt.plot(train_loss)
+    plt.plot(val_loss)
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'valid'], loc='upper left')
+    tikz_save(name+".tex")
